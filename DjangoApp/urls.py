a4fbe23a1ea02ad4f3c5579828b7c9870b50e148
Ralph <ralphcylex@gmail.com>
2022-05-05 23:53:27 +0000

    local commit of play

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from play.views import play
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/play', play, name='play'),
 ]
